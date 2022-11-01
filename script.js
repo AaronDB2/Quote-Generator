d91fd08f61025eca08c6f2746a81fa127084c6b7
Aaron de Boer <aarondeboerwk@gmail.com>
2022-11-01 16:34:54 +0100

    Added loading indicator when fetching or getting new random quote

--- script.js
+++ script.js
@@ -3,11 +3,25 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
+// Show loading
+function loading() {
+	loader.hidden = false;
+	quoteContainer.hidden = true;
+}
+
+// Hide loading
+function completeLoading() {
+	quoteContainer.hidden = false;
+	loader.hidden = true;
+}
+
 // Show new quote
 function newQuote() {
+	loading();
 	// Pick a random quote from apiQuotes array
 	const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
 
@@ -24,11 +38,15 @@ function newQuote() {
 	} else {
 		quoteText.classList.remove('long-quote');
 	}
+
+	// Set quote and hide loader
 	quoteText.textContent = quote.text;
+	completeLoading();
 }
 
 // Get quotes from API
 async function getQuotes () {
+	loading();
 	const apiURL = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
 	try {
 		const response = await fetch(apiURL);
@@ -40,7 +58,7 @@ async function getQuotes () {
 }
 
 // Tweet quote
-function tweetQuote(){
+function tweetQuote() {
 	const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
 	window.open(twitterUrl, '_blank');
 }
@@ -51,3 +69,4 @@ twitterBtn.addEventListener('click', tweetQuote);
 
 // On load
 getQuotes();
+
