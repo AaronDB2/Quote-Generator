8f4172cfb73c36aac67d985427aa928530b5f51a
Aaron de Boer <aarondeboerwk@gmail.com>
2022-11-01 15:48:04 +0100

    Fetched quotes from api and select one at random

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+let apiQuotes = [];
 
+// Show new quote
+function NewQuote() {
+	// Pick a random quote from apiQuotes array
+	const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
+	console.log(quote);
+}
 
-// https://jacintodesign.github.io/quotes-api/data/quotes.json
+// Get quotes from API
+async function GetQuotes () {
+	const apiURL = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+	try {
+		const response = await fetch(apiURL);
+		apiQuotes = await response.json();
+		NewQuote();
+	} catch (error) {
+		// Catch error here
+	}
+}
+
+// On load
+GetQuotes();
